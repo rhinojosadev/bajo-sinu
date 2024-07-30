6dafe6f18367006778a6f728991c08eb01dcb212
valdo <osvaldo@lvvdigital.com>
2024-07-29 19:36:12 -0600

    feat: add poc with chart.js

---
+++ src/pages/home/components/barChart.jsx
@@ -0,0 +1,33 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const BarChart = (props) => {
+  const data = props.data;
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  return <Bar data={data} options={options} />;
+};
