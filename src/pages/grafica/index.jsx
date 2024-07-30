4fa6dfba8eee310a1736089dcc3cd3b7cdd98ccb
valdo <osvaldo@lvvdigital.com>
2024-07-30 12:28:21 -0600

    feat: Consume API Agua2

--- src/pages/grafica/index.jsx
+++ src/pages/grafica/index.jsx
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { BarChart } from '../home/components/barChart';
+import { BarChart } from '../../components/grafica/BarChart';
 
 export const Grafica = () => {
-    const data = {
-        labels: ['2004', '2005', '2006', '2007', '2008', '2009'],
-        datasets: [
-            {
-                label: 'Car',
-                data: [77, 88, 99, 55, 44, 66],
-                borderColor: 'rgba(54, 162, 235, 1)',
-                backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            },
-            {
-                label: 'Leche',
-                data: [11, 12, 14, 11, 16, 12],
-                borderColor: 'rgba(255, 159, 64, 1)',
-                backgroundColor: 'rgba(255, 159, 64, 0.5)',
-            },
-            {
-                label: 'Carne',
-                data: [65, 59, 80, 81, 56, 55],
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.5)',
-            },
-        ]
-    };
+    const urls = {
+        bovinosCordoba: 'https://pruebas.correa.redhumus.org/agua2/api/bovinos/cordoba',
+    }
+
+    const [dataBovinosCordoba, setDataBovinosCordoba] = useState({
+        labels: [],
+        datasets: []
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(urls.bovinosCordoba);
+                const { labels, datasets } = await response.json();
+                setDataBovinosCordoba({ labels, datasets });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, [urls.bovinosCordoba]);
 
     return (
         <>
-            <p>grafica</p>
-            <BarChart data={data} />
+            <p>Graficas</p>
+
+            <>
+                <h1> Bovinos Cordoba </h1>
+                <div className='h-96'>
+                    <BarChart data={dataBovinosCordoba} />
+                </div>
+            </>
+            <>
+                <h1> Bovinos Cordoba 2</h1>
+                <div className='h-96'>
+                    <BarChart data={dataBovinosCordoba} />
+                </div>
+            </>
         </>
     )
 }
